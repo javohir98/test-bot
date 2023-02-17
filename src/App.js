d662b2b86a34c4d53e4734b4967b716ed08e62a1
javohir98 <jerkinov072@gmail.com>
2023-02-17 21:15:29 +0500

    #3

--- src/App.js
+++ src/App.js
@@ -19,12 +19,12 @@ const App = () => {
     <div>
       <button onClick={testInit}>testInit</button>
       <button onClick={testInitUserName}>testInitUserName</button>
-      <p>{tele.initDataUnsafe.user.id}</p>
-      <p>{tele.initDataUnsafe.user.isBot}</p>
-      <p>{tele.initDataUnsafe.user.first_name}</p>
-      <p>{tele.initDataUnsafe.user.last_name}</p>
-      <p>{tele.initDataUnsafe.user.username}</p>
-      <p>{tele.initDataUnsafe.user.language_code}</p>
+      <p>{tele.initDataUnsafe?.user?.id}</p>
+      <p>{tele.initDataUnsafe?.user?.isBot}</p>
+      <p>{tele.initDataUnsafe?.user?.first_name}</p>
+      <p>{tele.initDataUnsafe?.user?.last_name}</p>
+      <p>{tele.initDataUnsafe?.user?.username}</p>
+      <p>{tele.initDataUnsafe?.user?.language_code}</p>
     </div>
   )
 }
